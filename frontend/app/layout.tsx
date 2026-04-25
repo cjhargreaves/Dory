da5635929ffd4fe3859341a23be327d88251adbe
cjhargreaves <collin_hargreaves@berkeley.edu>
2026-04-25 05:19:00 -0700

    site docs

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
+        <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js" />
       </head>
       <body className="antialiased">{children}</body>
     </html>
